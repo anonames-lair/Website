5ac77f7b7575d8ecf029288e419e3c02eadc22a6
anonames-lair <latiassouldew@gmail.com>
2025-05-27 08:31:27 +0700

    Update energy and tool size and seperate scripts

---
+++ tcg/variables.js
@@ -0,0 +1,319 @@
+// Koaraidon Updated: https://pokemoncard.io/api/url-shorten/wBU
+// Generations Psychic Mix: https://pokemoncard.io/api/url-shorten/wGd
+// Zacian: https://asia.pokemon-card.com/id/deck-build/recipe/dXaRKy-WoCyfH-SwUipy/
+// Xerneas: https://asia.pokemon-card.com/id/deck-build/recipe/sveXei-VqZdFd-yHRuSo/
+
+const koraidon1 = {
+	'Koraidon EX': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Sandy Shocks': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Great Tusk': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Scream Tail': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Lumineon V': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Turtonator': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Rockruff': {
+		type: 'Pokemon',
+		count: 3
+	},
+	'Lycanroc': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Ponyta': {
+		type: 'Pokemon',
+		count: 3
+	},
+	'Rapidash': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Nest Ball': {
+		type: 'Item',
+		count: 2
+	},
+	'Ultra Ball': {
+		type: 'Item',
+		count: 4
+	},
+	'Master Ball': {
+		type: 'Item',
+		count: 1
+	},
+	'Earthen Vessel': {
+		type: 'Item',
+		count: 1
+	},
+	'Switch': {
+		type: 'Item',
+		count: 2
+	},
+	'Ancient Booster Energy': {
+		type: 'Tool',
+		count: 2
+	},
+	'Cynthia Ambition': {
+		type: 'Supporter',
+		count: 2
+	},
+	'Professor Sada Vitality': {
+		type: 'Supporter',
+		count: 2
+	},
+	'Professor Research': {
+		type: 'Supporter',
+		count: 4
+	},
+	'Nemona': {
+		type: 'Supporter',
+		count: 3
+	},
+	'Iono': {
+		type: 'Supporter',
+		count: 2
+	},
+	'Boss Orders': {
+		type: 'Supporter',
+		count: 2
+	},
+	'Fighting Energy': {
+		type: 'Energy',
+		count: 8
+	},
+	'Fire Energy': {
+		type: 'Energy',
+		count: 7
+	}
+};
+
+const koraidon2 = {
+	'Koraidon EX': {
+		type: 'Pokemon',
+		count: 4
+	},
+	'Koraidon A': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Koraidon S': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Sandy Shocks': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Flutter Mane': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Latias EX': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Radiant Greninja': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Fezandipiti EX': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Lumineon V': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Nest Ball': {
+		type: 'Item',
+		count: 4
+	},
+	'Ultra Ball': {
+		type: 'Item',
+		count: 4
+	},
+	'Quick Ball': {
+		type: 'Item',
+		count: 1
+	},
+	'Master Ball': {
+		type: 'Item',
+		count: 1
+	},
+	'Earthen Vessel': {
+		type: 'Item',
+		count: 4
+	},
+	'Trekking Shoes': {
+		type: 'Item',
+		count: 3
+	},
+	'Gutsy Pickaxe': {
+		type: 'Item',
+		count: 1
+	},
+	'Ancient Booster Energy': {
+		type: 'Tool',
+		count: 1
+	},
+	'Explorer Guidance': {
+		type: 'Supporter',
+		count: 4
+	},
+	'Professor Sada Vitality': {
+		type: 'Supporter',
+		count: 4
+	},
+	'Boss Orders': {
+		type: 'Supporter',
+		count: 2
+	},
+	'Cycling Road': {
+		type: 'Stadium',
+		count: 1
+	},
+	'Fighting Energy': {
+		type: 'Energy',
+		count: 11
+	},
+	'Fire Energy': {
+		type: 'Energy',
+		count: 4
+	}
+};
+
+const psychicMix = {
+	'Noibat': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Noivern EX': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Milcery': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Alcremie EX': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Xerneas EX': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Eevee': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Sylveon': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Impidimp': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Morgrem': {
+		type: 'Pokemon',
+		count: 2
+	},
+	'Grimmsnarl': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Dedenne': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Galarian Stunfisk': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Furfrou': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Hawlucha': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Indeedee': {
+		type: 'Pokemon',
+		count: 1
+	},
+	'Great Ball': {
+		type: 'Item',
+		count: 4
+	},
+	'Ultra Ball': {
+		type: 'Item',
+		count: 4
+	},
+	'Pokemon Catcher': {
+		type: 'Item',
+		count: 2
+	},
+	'Switch': {
+		type: 'Item',
+		count: 2
+	},
+	'Nemona': {
+		type: 'Supporter',
+		count: 4
+	},
+	'Professor Research': {
+		type: 'Supporter',
+		count: 3
+	},
+	'Judge': {
+		type: 'Supporter',
+		count: 2
+	},
+	'Youngster': {
+		type: 'Supporter',
+		count: 1
+	},
+	'Psychic Energy': {
+		type: 'Energy',
+		count: 17
+	},
+	'Therapeutic Energy': {
+		type: 'Energy',
+		count: 1
+	}
+};
+
+const ratio = window.devicePixelRatio;
+const canvasWidth = 122 * ratio;
+const canvasHeight = 171 * ratio;
+const styleWidth = 122 + 'px';
+const styleHeight =	171 + 'px';
+const energySize = 18 * ratio;
+const energySpacing = 3;
+const radius = energySize / 2;
+const toolWidth = 40 * ratio;
+const toolHeight = 20 * ratio;
+const toolX = (canvasWidth - toolWidth) / 2;
+const toolY = (canvasHeight - toolHeight) / 2;
+var printMode = 1;
+
+const startingHand = 7;
+const prizeCount = 6;
+var benchSize = 5;
